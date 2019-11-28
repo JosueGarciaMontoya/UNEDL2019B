e7f9db48b9f63420990872de8b5bb7a215d630bc
josue <josue.alex72@gmail.com>
2019-11-27 23:43:55 -0600

    Examen2doParcial

--- console/TPV2doParcial/Examen_Taller_2do_Parcial/Properties/Settings.Designer.cs
+++ console/TPV2doParcial/Examen_Taller_2do_Parcial/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ExamenTaller2doParcial.Properties
+namespace Examen_Taller_2do_Parcial.Properties
 {
 
 
